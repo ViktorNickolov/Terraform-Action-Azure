685062d4a4de1972df6dbd61e9eb36f2868297f0
Viktor Nikolov <viktor.nickolov@gmail.com>
2024-12-09 20:30:16 +0200

    fmt

--- main.tf
+++ main.tf
@@ -89,8 +89,8 @@ resource "azurerm_linux_web_app" "lwa" {
 }
 
 resource "azurerm_app_service_source_control" "sc" {
-  app_id   = azurerm_linux_web_app.lwa.id
-  repo_url = var.GitHub_repo_URL
+  app_id                 = azurerm_linux_web_app.lwa.id
+  repo_url               = var.GitHub_repo_URL
   branch                 = "master"
   use_manual_integration = true
 }
